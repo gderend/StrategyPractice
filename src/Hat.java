ccba27898509f95a745dbd24052feae796083885
gul <gulderendav@gmail.com>
2023-01-04 15:34:16 -0800

    updated to hat class, feature hepsinde varmis

--- src/Hat.java
+++ src/Hat.java
@@ -1,6 +1,7 @@
 public class Hat {
     public static void main(String[] args) {
         System.out.println("bu sadece feature b de goruluyor");
+        System.out.println("esasinda hepsinde hat class goruluyormus");
 
     }
 }
