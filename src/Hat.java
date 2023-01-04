68fe9e76400170e99d251f58a0a02f8dd6d01886
gul <gulderendav@gmail.com>
2023-01-04 15:36:23 -0800

    updated to hat class,son commit

--- src/Hat.java
+++ src/Hat.java
@@ -2,6 +2,7 @@ public class Hat {
     public static void main(String[] args) {
         System.out.println("bu sadece feature b de goruluyor");
         System.out.println("esasinda hepsinde hat class goruluyormus");
+        System.out.println("push etmeden onceki change commit etme");
 
     }
 }
