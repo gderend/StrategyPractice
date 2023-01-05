0e5edca7789e85989019319624eee958e5626723
gul <gulderendav@gmail.com>
2023-01-04 16:59:37 -0800

    store class added new change feature c after updated

--- src/Store1.java
+++ src/Store1.java
@@ -1,5 +1,6 @@
 public class Store1 {
     public static void main(String[] args) {
         System.out.println("One change");
+        System.out.println("Feature c yi updated ettim simdi  yeni line add");
     }
 }
